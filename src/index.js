ae3afb2e30e8615b89cce07f7fe26ceaeaa592cc
João Maciel <joaomaciel.n@gmail.com>
2018-03-11 22:53:16 -0300

    adds apollo

--- src/index.js
+++ src/index.js
@@ -1,26 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createBrowserHistory } from 'history';
-import {
-    Router,
-    Route,
-    Switch
-} from 'react-router-dom';
+import { Router, Route, Switch } from 'react-router-dom';
 import 'assets/css/material-dashboard-react.css';
 import indexRoutes from 'routes/index.jsx';
 
 const hist = createBrowserHistory();
 
 ReactDOM.render(
-    <Router history={hist}>
-        <Switch>
-            {
-                indexRoutes.map((prop,key) => {
-                    return (
-                        <Route path={prop.path} component={prop.component}  key={key}/>
-                    );
-                })
-            }
-        </Switch>
-    </Router>
+  <Router history={hist}>
+    <Switch>
+      {indexRoutes.map((prop, key) => <Route path={prop.path} component={prop.component} key={key}/>)}
+    </Switch>
+  </Router>
 , document.getElementById('root'));
