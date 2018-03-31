4470f0bbc77fe9f17160935b03a6ba7da306c9df
Jon Maciel <joaomaciel.n@gmail.com>
2018-03-31 00:33:52 -0300

    imporoves de footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,40 +1,44 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {
-    List, ListItem, withStyles
-} from 'material-ui';
+import { List, ListItem, withStyles } from 'material-ui';
 import { Link } from 'react-router-dom';
 
 import { footerStyle } from 'variables/styles';
 
 class Footer extends React.Component{
-    render(){
-        const { classes } = this.props;
-        return (
-            <footer className={classes.footer}>
-                <div className={classes.container}>
-                    <div className={classes.left}>
-                        <List className={classes.list}>
-                            <ListItem className={classes.inlineBlock}>
-                                <Link to="/videos" className={classes.block}>Vídeos</Link>
-                            </ListItem>
-                        </List>
-                    </div>
-                    <p className={classes.right}>
-                        <span>
-                            &copy; { 1900 + (new Date()).getYear()}
-                            <a href="http://www.tutorbox.com.br" target="_blank" className={classes.a}>Tutorbox</a>,
-                            produzimos e mantemos atualizados os vídeos tutoriais de seu software.
-                        </span>
-                    </p>
-                </div>
-            </footer>
-        );
-    }
+  render(){
+    const { classes } = this.props;
+    return (
+      <footer className={classes.footer}>
+        <div className={classes.container}>
+          <div className={classes.left}>
+            <List className={classes.list}>
+              <ListItem className={classes.inlineBlock}>
+                <Link to="/tutormakers" className={classes.block}>Tutormakers</Link>
+              </ListItem>
+              <ListItem className={classes.inlineBlock}>
+                <Link to="/organizations" className={classes.block}>Organizações</Link>
+              </ListItem>
+              <ListItem className={classes.inlineBlock}>
+                <Link to="/videos" className={classes.block}>Vídeos</Link>
+              </ListItem>
+            </List>
+          </div>
+          <p className={classes.right}>
+            <span>
+              &copy; { 1900 + (new Date()).getYear()}
+              <a href="http://www.tutorbox.com.br" target="_blank" className={classes.a}>Tutorbox</a>,
+              produzimos e mantemos atualizados os vídeos tutoriais de seu software.
+            </span>
+          </p>
+        </div>
+      </footer>
+    );
+  }
 }
 
 Footer.propTypes = {
-    classes: PropTypes.object.isRequired
+  classes: PropTypes.object.isRequired
 };
 
 export default withStyles(footerStyle)(Footer);
