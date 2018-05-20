2e2c094b1e7883ca169947b76e48d4cbfd8cfc84
Jon Maciel <joaomaciel.n@gmail.com>
2018-05-20 15:46:14 -0300

    Fixes task list to refetch on did mount

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,33 +1,49 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Typography } from 'material-ui';
 import { RegularCard, Tasks } from 'components';
 import { graphql, compose } from 'react-apollo';
 import gql from 'graphql-tag';
 
-const TaskList = ({ videoId, data: { tasks, refetch } }) => {
+class TaskList extends Component {
+  componentDidMount() {
+    this.props.data.refetch();
+  }
 
-  if(!tasks) { return <div /> }
+  tasksParams = (tasks) =>
+    tasks.reduce((prevTasks, task) => {
+      if(task.done) {
+        prevTasks.checkedIndexes.push(task.id)
+      }
 
-  const tasksParams = tasks.reduce((prevTasks, task) => {
-    if(task.done) {
-      prevTasks.checkedIndexes.push(task.id)
-    }
+      prevTasks.tasksIndexes.push(task.id)
+      prevTasks.tasks[task.id] = task.name
 
-    prevTasks.tasksIndexes.push(task.id)
-    prevTasks.tasks[task.id] = task.name
+      return prevTasks;
+    }, { checkedIndexes: [], tasksIndexes: [], tasks: {} });
 
-    return prevTasks;
-  }, { checkedIndexes: [], tasksIndexes: [], tasks: {} } );
 
-  return <RegularCard
-    headerColor="green"
-    cardTitle="Tarefas"
-    content={
-      <Typography component="div">
-        <Tasks {...tasksParams} refetchTasks={refetch} videoId={videoId} />
-      </Typography>
-  } />
+  render() {
+   const { videoId, data: { tasks, refetch } } = this.props;
+
+    if(!tasks) { return <div /> }
+
+    return (
+      <RegularCard
+        headerColor="green"
+        cardTitle="Tarefas"
+        content={
+          <Typography component="div">
+            <Tasks
+              {...this.tasksParams(tasks)}
+              refetchTasks={refetch}
+              videoId={videoId}
+            />
+          </Typography>
+        }
+       />
+    )
+  }
 }
 
 TaskList.propTypes = {
